f5764b1ab9af59414a81fb38a2b720140834be67
l.vanlaake@qquest.nl <l.vanlaake@qquest.nl>
2018-05-23 10:39:59 +0200

    initial commit

--- script.sh
+++ script.sh
@@ -1,8 +1,10 @@
+cd lennartatqquest
 for folder in $(ls); do
+  echo $folder
   cd $folder
   git add -A
   git commit -m "initial commit"
-  git remote add origin https://github.com/lennartvanlaake/$(folder).git
+  git remote add origin https://github.com/lennartvanlaake/${folder}.git
   git push -u origin master
   cd ..
 done
